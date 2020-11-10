0ac04538a9a853b1b8abdeab5c6ab2eb3f962878
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-10 09:12:16 +0530

    Added: Binary Node in Binary Search Tree

--- src/main/java/BinaryNode.java
+++ src/main/java/BinaryNode.java
@@ -1,36 +1,13 @@
 
 public class BinaryNode<K extends Comparable<K>> {
-	private K key;
-	private BinaryNode<K> left;
-	private BinaryNode<K> right;
+	K key;
+	BinaryNode<K> left;
+	BinaryNode<K> right;
 	
 	public BinaryNode(K key) {
-		this.setKey(key);
-		this.setLeft(null);
-		this.setRight(null);
-	}
-
-	public K getKey() {
-		return key;
-	}
-
-	public void setKey(K key) {
 		this.key = key;
+		this.left = null;
+		this.right = null;
 	}
-
-	public BinaryNode<K> getLeft() {
-		return left;
-	}
-
-	public void setLeft(BinaryNode<K> left) {
-		this.left = left;
-	}
-
-	public BinaryNode<K> getRight() {
-		return right;
-	}
-
-	public void setRight(BinaryNode<K> right) {
-		this.right = right;
-	}
+	
 }
--- src/test/java/BinarySearchTreeTest.java
+++ src/test/java/BinarySearchTreeTest.java
@@ -8,10 +8,10 @@ public class BinarySearchTreeTest {
 		BinaryNode<Integer> firstBinaryNode = new BinaryNode<>(56);
 		BinaryNode<Integer> secondBinaryNode = new BinaryNode<>(30);
 		BinaryNode<Integer> thirdBinaryNode = new BinaryNode<>(70);
-		firstBinaryNode.setLeft(secondBinaryNode);
-		firstBinaryNode.setRight(thirdBinaryNode);
-		boolean result = firstBinaryNode.getLeft().equals(secondBinaryNode) &&
-						firstBinaryNode.getRight().equals(thirdBinaryNode);
+		firstBinaryNode.left = secondBinaryNode;
+		firstBinaryNode.right = thirdBinaryNode;
+		boolean result = firstBinaryNode.left.equals(secondBinaryNode) &&
+				firstBinaryNode.right.equals(thirdBinaryNode);
 		Assert.assertTrue(result);
 	}
 }
