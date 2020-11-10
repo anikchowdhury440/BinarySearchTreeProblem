ef1d40205e24e314f409f9f834b3f7ed65b32c49
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-10 09:27:03 +0530

    Added: Binary Search Tree Class

---
+++ src/main/java/BinarySearchTree.java
@@ -0,0 +1,40 @@
+
+public class BinarySearchTree<K extends Comparable<K>>{
+	private BinaryNode<K> root;
+	
+	public void add(K key) {
+		this.root = this.addRecursively(root, key); 
+	}
+	
+	private BinaryNode<K> addRecursively(BinaryNode<K> current, K key) {
+		if(current == null) {
+			return new BinaryNode<K>(key);
+		}
+		int compareResult = key.compareTo(current.key);
+		if (compareResult == 0) {
+			return current;
+		}
+		else if (compareResult < 0) {
+			current.left = addRecursively(current.left, key);
+		}
+		else {
+			current.right = addRecursively(current.right, key);
+		}
+		return current;
+	}
+	
+	public int size() {
+		return this.getSizeRecursive(root);
+	}
+
+	private int getSizeRecursive(BinaryNode<K> current) {
+		if(current == null) {
+			return 0;
+		}
+		else {
+			return 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+		}
+	}
+	
+	
+}
--- src/test/java/BinarySearchTreeTest.java
+++ src/test/java/BinarySearchTreeTest.java
@@ -14,4 +14,24 @@ public class BinarySearchTreeTest {
 				firstBinaryNode.right.equals(thirdBinaryNode);
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void checkValues_AddedInBST_UsingSizeinBST() {
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(11);
+		binarySearchTree.add(65);
+		binarySearchTree.add(3);
+		binarySearchTree.add(16);
+		binarySearchTree.add(63);
+		binarySearchTree.add(67);
+		int size = binarySearchTree.size();
+		Assert.assertEquals(13, size);
+	}
 }
